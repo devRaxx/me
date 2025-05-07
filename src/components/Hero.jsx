3f6b93ac199d06befc884cb00271f50d15fd9dda
devRaxx <rpcastaneros@up.edu.ph>
2025-05-07 23:46:29 +0800

    feat: added montserrat font

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <div>
+      <section>
+        <div>
+          <h1>Hi, I&apos;m Rax!</h1>
+          <p>
+            I'm a front-end developer passionate about building fast,
+            accessible, and user-centered web experiences. With hands-on
+            experience in React, Next.js, and Tailwind, I’ve shipped scalable
+            solutions for startups, student organizations, and tech communities.
+            Whether it's automating event platforms, redesigning high-traffic
+            pages, or creating interactive UI for civic tech, I bring clean code
+            and thoughtful design to every project.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
