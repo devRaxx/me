b659790b25cffb634c1f3e0ec47349a110ca6a5d
devRaxx <rpcastaneros@up.edu.ph>
2025-05-08 06:02:55 +0800

    feat: added Work.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Work from "./components/Work";
 function App() {
   return (
     <div className="font-montserrat">
       <Hero />
       <Projects />
+      <Work />
     </div>
   );
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,11 @@
+import Cards from "../ui/Cards";
+
+export default function Work() {
+  return (
+    <section className="h-screen">
+      <div className="flex flex-col items-center justify-center text-center mx-auto mt-14">
+        WORK SECTION
+      </div>
+    </section>
+  );
+}
